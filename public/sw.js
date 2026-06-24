26831eb99f1e3fc90e3966e7884a3ce7a0a3a1d6
Zack-River <elmutamiz@gmail.com>
2026-06-24 05:24:08 +0300

    Fix: Rewrite Service Worker to unregister itself and clear old index.html cache

--- public/sw.js
+++ public/sw.js
@@ -1,45 +1,23 @@
 // public/sw.js
-const CACHE_NAME = 'smartq-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/vite.svg',
-  '/manifest.json'
-];
+// This Service Worker is designed to kill the old cache and unregister itself.
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+});
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          return caches.delete(cacheName);
+        })
+      );
+    }).then(() => {
+      return self.registration.unregister();
+    })
   );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
+  // Do nothing, let the browser handle it normally.
 });
